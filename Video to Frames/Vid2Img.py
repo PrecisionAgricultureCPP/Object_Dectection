6e5bf3c27dc7d2d97896e73f6e598d6225677a78
Marco Mercado <mmerc007@ucr.edu>
2019-06-27 22:49:08 -0700

    push

--- Video to Frames/Vid2Img.py
+++ Video to Frames/Vid2Img.py
@@ -1,27 +1,23 @@
 import cv2
-from tkinter.filedialog import askopenfilename
+import os
+# from tkinter.filedialog import askopenfilename
+from tkFileDialog import askopenfilename
 
-filename = askopenfilename()
-origTokens = filename.split('/')
-origFile = origTokens[-1]
-justNameTokens = origFile.split('.')
-justName = justNameTokens[0]
-print(justName)
-
-video = cv2.VideoCapture(filename)
-if not video.isOpened():
-    print("file not valid")
-    exit()
-success, image = video.read()
-count = 0
-success = True
-while success:
-    cv2.imwrite(justName + "-frame%d.jpg" % count, image)
+AbsolutePath=os.getcwd()
+file = open("nameOfVideos.txt", "r")
+for filename in file:
+    filename=filename.strip()
+    VideoPath=os.path.join(AbsolutePath,"Videos", filename)
+    video = cv2.VideoCapture(VideoPath)
+    if not video.isOpened():
+        print("file not valid")
+        exit()
     success, image = video.read()
-    if success:
-        print('Reading frame', count+1)
-    count += 1
-
-
-
-
+    count = 0
+    success = True
+    while success:
+        cv2.imwrite(os.path.join(AbsolutePath,"Images",filename + "-frame%d.jpg" % count), image)
+        success, image = video.read()
+        if success:
+            print('Reading frame', count+1)
+        count += 1
